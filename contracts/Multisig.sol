da4f48d2650d450fd066d13969fc2d63ef1013f0
Thileepan-Ilankumaran <thileep.ilankumaran@gmail.com>
2023-03-27 02:16:05 +0530

    Added function execute

--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -10,38 +10,36 @@ contract Multisig is AccessControl {
         bool approved;
     }
     uint public proposalCount;
-    mapping(address => bool) public hasVoted;
-    mapping(uint256 => mapping(address => bool)) public confirmations;
-    mapping(uint => Proposal) proposals;
+    uint public votesCount;
+    mapping(uint256 => mapping(address => bool)) public hasVoted; //
+    mapping(uint => Proposal) proposals; //0,1,2,3
+    mapping(uint => uint) public votes;
 
     Proposal[] totalProposals;
-    Proposal[] validProposals;
 
-    modifier onlyOwner(address _owners) {
-        require(isOwner[_owners] == true, "Only owner can call this function");
+    modifier onlyOwner(address owner) {
+        require(isOwner[owner] == true, "Only owner can call this function");
         _;
     }
 
-    // modifier isApproved(uint proposalId, address recipient) {
-    //     require(
-    //         confirmations[proposalId][recipient] == true,
-    //         "Proposal already approved"
-    //     );
-    //     _;
-    // }
-
-    modifier isVoted(uint proposalId, address _owners) {
-        require(hasVoted[_owners] == false, "Already Voted");
+    modifier isApproved(uint proposalId) {
+        require(
+            totalProposals[proposalId].approved == false,
+            "Already approved"
+        );
         _;
     }
 
-    constructor(address[] memory _owners) AccessControl(_owners) {}
+    modifier isVoted(uint proposalId, address owner) {
+        require(hasVoted[proposalId][owner] == false, "Already Voted");
+        _;
+    }
 
     function createProposal(
         address _recipent,
         uint _value,
         string memory _proposal
-    ) public returns (uint256 proposalId) {
+    ) public payable returns (uint256 proposalId) {
         proposalId = proposalCount;
         proposals[proposalId] = Proposal({
             recipient: _recipent,
@@ -51,25 +49,40 @@ contract Multisig is AccessControl {
         });
         totalProposals.push(proposals[proposalId]);
         proposalCount += 1;
-        emit Submission(proposalId);
     }
 
-    function approval(uint proposalId) public onlyAdmin {
-        require(
-            validProposals[proposalId].approved == false,
-            "Already approved"
-        );
-        validProposals.push(proposals[proposalId]);
-        validProposals[proposalId].approved = true;
-        // confirmations[proposalId][recipient] = true;
+    function approval(uint proposalId) public isApproved(proposalId) onlyAdmin {
+        proposalId = proposalCount;
+        totalProposals[proposalId].approved = true;
     }
 
     function voting(
         uint proposalId,
-        address _owners
-    ) public onlyOwner(msg.sender) isVoted(proposalId, msg.sender) {
-        hasVoted[_owners] = true;
+        address owners
+    ) public isVoted(proposalId, msg.sender) onlyOwner(msg.sender) {
+        require(
+            totalProposals[proposalId].approved == true,
+            "Only approved proposals can be voted"
+        );
+        hasVoted[proposalId][owners] = true;
+        votes[proposalId]++;
+        votesCount++;
+        execute();
     }
 
-    function execute() public {}
+    function execute() public {
+        //winning proposal
+        uint winner = votes[0];
+        for (uint i = 0; i < proposalCount; i++) {
+            if (votes[i] > winner) {
+                winner = votes[i];
+            }
+        }
+        //51%
+        uint num = votesCount * 51;
+        uint quorum = num / 100;
+
+        require(winner >= quorum, "Atleast 51% votes required to execute");
+        //withdraw
+    }
 }
